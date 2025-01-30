a50995bca8cb57ce5caa0f79562a452f20bc9936
Rajeev Tapadia <108951750+rajeevtapadia@users.noreply.github.com>
2025-01-30 23:18:37 +0530

    train single neuron with two inputs and single output

---
+++ neuron-2i-1o.c
@@ -0,0 +1,93 @@
+/*
+ * training a neruon with two two inputs, single output and no bias
+ *
+ * result: loss = 0.0833 after 40k iterations, 
+ *         became very difficult to reduce loss futher,
+ *         possible solution use sigmoid
+ *
+ * output:
+ * intitial weights (randomly selected)
+ * w1: 0.840188 w2: 0.394383
+ * after training weights - 
+ * w1: 0.667061 w2: 0.666929
+ * x1 x2 y pred
+ * 0  0  0 0.000000 
+ * 1  0  1 0.667061 
+ * 0  1  1 0.666929 
+ * 1  1  1 1.333990 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 4
+
+typedef struct {
+    float w1;
+    float w2;
+} weight_t;
+
+int df[][3] = {{0, 0, 0}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1}};
+
+float get_rand() {
+    // srand(time(0));
+    return (float)rand() / RAND_MAX;
+}
+
+void print_predictions(float w1, float w2) {
+    for (int i = 0; i < SIZE; i++) {
+        float x1 = df[i][0];
+        float x2 = df[i][1];
+        float y = df[i][2];
+        float pred = w1 * x1 + w2 * x2;
+        float diff = pred - y;
+        printf("%f %f %f %f %f\n", x1, x2, y, pred, diff);
+    }
+}
+
+float rms_error(float w1, float w2) {
+    float result = 0;
+    for (int i = 0; i < SIZE; i++) {
+        float x1 = df[i][0];
+        float x2 = df[i][1];
+        float y = df[i][2];
+        float pred = w1 * x1 + w2 * x2;
+        float diff = pred - y;
+        result += diff * diff;
+    }
+    result /= SIZE;
+    return result;
+}
+
+weight_t train(float w1, float w2) {
+    float rate = 1e-3;
+    float eps = 1e-3;
+    weight_t weights;
+    
+    for (int i = 0; i < 40000; i++) {
+        float vim = rms_error(w1, w2);
+        // printf("loss func value: %f\n", vim);
+        float slope_w1 = (rms_error(w1 - eps, w2) - vim) / eps;
+        float slope_w2 = (rms_error(w1, w2 - eps) - vim) / eps;
+
+        // printf("slope_w1: %f slope_w2: %f\n", slope_w1, slope_w2);
+        w1 += slope_w1 * rate;
+        w2 += slope_w2 * rate;
+        // printf("updated weights - w1: %f w2: %f\n", w1, w2);
+        // printf("updated loss func value: %f\n", rms_error(w1, w2));
+        weights.w1 = w1;
+        weights.w2 = w2;
+    }
+    return weights;
+}
+
+int main() {
+    float w1 = get_rand();
+    float w2 = get_rand();
+    printf("initial weights\n%f, %f\n", w1, w2);
+    weight_t w = train(w1, w2);
+    printf("updated weights - w1: %f w2: %f\n", w.w1, w.w2);
+    print_predictions(w.w1, w.w2);
+    return 0;
+}
